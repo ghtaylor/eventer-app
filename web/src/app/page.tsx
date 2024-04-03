960bed9624c97a6ec3cb979ebd7cd09885dc78ac
George Taylor <ghtaylor@outlook.com>
2024-04-03 20:21:59 +0100

    feat: introduce forward ref with class name merging

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
         </div>
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {events.map((event) => (
-            <EventListItem key={event.id} event={event} />
+            <EventListItem className="h-96 md:h-[32rem]" key={event.id} event={event} />
           ))}
         </ul>
       </section>
--- web/src/components/main/EventListItem.tsx
+++ web/src/components/main/EventListItem.tsx
@@ -1,26 +1,31 @@
-import { Event, EventWithTickets } from "@kaboodle-events-app/db/schema";
+import { cn } from "@/lib/utils";
+import { EventWithTickets } from "@kaboodle-events-app/db/schema";
 import Image from "next/image";
-import { memo, useMemo } from "react";
+import { forwardRef, useMemo } from "react";
 
 interface EventListItemProps {
   event: EventWithTickets;
 }
 
-const EventListItem: React.FC<EventListItemProps> = ({ event }) => {
-  const formattedDate = useMemo(
-    () => new Date(event.date).toLocaleDateString("en-GB", { day: "2-digit", month: "long", year: "numeric" }),
-    [event.date],
-  );
+const EventListItem = forwardRef<HTMLLIElement, React.HTMLAttributes<HTMLLIElement> & EventListItemProps>(
+  ({ event, className, ...props }, ref) => {
+    const formattedDate = useMemo(
+      () => new Date(event.date).toLocaleDateString("en-GB", { day: "2-digit", month: "long", year: "numeric" }),
+      [event.date],
+    );
 
-  return (
-    <li className="relative h-96 w-full overflow-hidden">
-      <Image src={`/${event.id}.jpg`} alt={event.name} layout="fill" objectFit="cover" />
-      <div className="absolute z-10 w-full bottom-0 p-4 bg-gradient-to-t from-gray-900 to-transparent">
-        <span className="text-white uppercase">{formattedDate}</span>
-        <h2 className="text-white text-5xl">{event.name}</h2>
-      </div>
-    </li>
-  );
-};
+    return (
+      <li ref={ref} className={cn("relative w-full overflow-hidden", className)} {...props}>
+        <Image src={`/${event.id}.jpg`} alt={event.name} layout="fill" objectFit="cover" />
+        <div className="absolute z-10 w-full bottom-0 p-4 bg-gradient-to-t from-gray-900 to-transparent">
+          <span className="text-white uppercase">{formattedDate}</span>
+          <h2 className="text-white text-5xl">{event.name}</h2>
+        </div>
+      </li>
+    );
+  },
+);
+
+EventListItem.displayName = "EventListItem";
 
 export default EventListItem;
