7a13eccca8b6f282ed304af98d93d59e15b52c68
George Taylor <ghtaylor@outlook.com>
2024-04-03 09:58:17 +0100

    chore: basic eslint prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./api/tsconfig.json", "./events/tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
