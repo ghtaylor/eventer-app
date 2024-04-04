f9762897d8cd23293c8bd3d8f024ec9f4308bbcb
George Taylor <ghtaylor@outlook.com>
2024-04-04 11:07:52 +0100

    feat: add home link to navbar title

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 import { LuMenu } from "react-icons/lu";
 
 const Navbar: React.FC = () => (
   <nav className="absolute z-10  p-4 w-full flex justify-between">
-    <h1 className="text-3xl font-bold tracking-tighter">Eventer</h1>
+    <h1 className="text-3xl font-bold tracking-tighter">
+      <Link href="/">Eventer</Link>
+    </h1>
     <div>
       <LuMenu className="text-4xl" />
     </div>
