7189679bef7c473ecb5e15b85d6e55f6eef19300
George Taylor <ghtaylor@outlook.com>
2024-04-04 10:50:05 +0100

    fix: currency in purchase ticket list item displays correctly

--- web/src/components/core/PurchaseTicketListItem.tsx
+++ web/src/components/core/PurchaseTicketListItem.tsx
@@ -23,8 +23,8 @@ const PurchaseTicketListItem = forwardRef<
       <span className="text-3xl font-bold w-6">{selectedCount}</span>
       <div className="flex-grow">
         <h3 className="text-lg font-bold">{ticket.type}</h3>
-        <span className="block">£{ticketPrice}</span>
-        <span className="text-sm italic block">+ £{ticketBookingFee} booking fee</span>
+        <span className="block">£{ticketPrice.toFixed(2)}</span>
+        <span className="text-sm italic block">+ £{ticketBookingFee.toFixed(2)} booking fee</span>
       </div>
       <div>
         <Button variant="secondary" onClick={decrementCount}>
