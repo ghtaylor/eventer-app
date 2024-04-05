e9733d635e3ae044b489d468616c395b07e97dd6
George Taylor <ghtaylor@outlook.com>
2024-04-05 11:18:48 +0100

    chore: change frontend title in metadata

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kaboodle Events",
+  title: "Eventer",
   description: "Make your event unforgettable.",
 };
 
